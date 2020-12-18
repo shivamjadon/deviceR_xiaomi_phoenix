6d523f3c34027601d099d55ffcb37974daaffbf8
karthik558 <karthik.lal558@gmail.com>
2020-12-18 15:26:32 +0000

    phoenix: Add props to improve battery backup
    Signed-off-by: chiranth <chiranthangadi123@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -36,3 +36,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Priv-app permission
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.control_privapp_permissions=log
+
+# Power-saving props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.use_data_netmgrd=true \
+    ro.vold.umsdirtyratio=20 \
+    ro.ril.disable.power.collapse=0 \
+    power.saving.mode=1 \
+    pm.sleep_mode=1
